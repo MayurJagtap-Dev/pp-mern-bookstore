28955102c89571418d2f9990e8fd67fe3f95693d
Mayur Jagtap <mayurjagtap9112@gmail.com>
2024-03-10 22:42:29 +0530

    user schema

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
