56d338833d7d469be8541377ad9d80ae5fbb63c6
Mayur Jagtap <mayurjagtap9112@gmail.com>
2024-03-23 21:45:40 +0530

    Complete UI update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
-function App() {
-  return <div>Hello</div>;
-}
+import { Outlet } from "react-router-dom";
+import Navigation from "./pages/Auth/Navigation";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  return (
+    <>
+      <ToastContainer />
+      <Navigation />
+      <main className="py-3">
+        <Outlet />
+      </main>
+    </>
+  );
+};
 
 export default App;
